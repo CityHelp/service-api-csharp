6f1243d50fff68ca08eaa8c0c05c478332132a26
Juan Jose Hernandez <juaan.josehernandez@gmail.com>
2025-11-30 16:57:45 -0500

    chore:add configuration for database and enviroments variables

--- service-api-csharp.API/Program.cs
+++ service-api-csharp.API/Program.cs
@@ -1,10 +1,23 @@
+using service_api_csharp.Infrastructure;
+using DotNetEnv;
+using service_api_csharp.Application;
+
+Env.Load();
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+var services = builder.Services;
+var configuration = builder.Configuration;
+
+services.AddInfrastructureServices(configuration);
+services.AddApplicationDI();
 
 var app = builder.Build();
+
+// builder.Services.AddControllers();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -14,28 +27,5 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
